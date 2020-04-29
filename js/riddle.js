40f19f23ed115284b15bcb96d25037136d7e1fc1
Vo Thuong <vot@ismanila.org>
2020-04-29 09:51:13 +0800

    Edited daily puzzle

--- js/riddle.js
+++ js/riddle.js
@@ -1,17 +1,30 @@
-var solutionContainer = document.getElementById("solution-container");
 var problemContainer = document.getElementById("problem-container");
-
+var solutionButton = document.getElementById("solution-button");
+var solutionContainer = document.getElementById("solution-container");
 solutionContainer.style.display = "none";
 
 var problems = {
-    "29-4-2020": "<p> If an acute triangle is divided into 3 triangles and 1 quadrilateral as shown below, what is the area of the quadrilateral ? </p> <img src='../images/puzzle/29-4-2020.png'/>",
+    "29-4-2020": "<p> If an acute triangle is divided into 3 triangles and 1 quadrilateral as shown below, what is the area of the quadrilateral ? </p> <img src='../images/puzzle/29-4-2020.png' width='240' height='162'/>",
 };
 var solutions = {
-    "29-4-2020": "<p>18</p>"
+    "29-4-2020": `<p>The quadrilateral has area 18 units squared. First, draw a line from the tip of the quadrilateral to its bottom and label the points, like so: <br/></p>
+    <img src='../images/puzzle/29-4-2020-solution.png/' width='240' height='162'/><br/><p>AF = FD because BF is a median of triangle ABD.<br/>
+    EF = 3 ⋅ FB : 7, as the ratio of the areas of triangle AEF and ABF is 3 : 7. As AF = FD, x + 3 = y. <br/>
+    Because EF = 3 ⋅ FB : 7, x : y + 7 = 3 : 7. <br/>
+    Using substitution, x : x + 10 = 3 : 7. 7x = 3x + 30, and x = 15 : 2. <br/>
+    y = 21 : 2, so x + y = the area of quadrilateral CEFD = 15 + 21 : 2 = 18.</p>`,
 };
 
 var date = new Date();
-var today = (date.getDate()+1) + "-" + (date.getMonth() + 1)  + "-" + date.getFullYear();
+var today = date.getDate() + "-" + (date.getMonth() + 1)  + "-" + date.getFullYear();
 
 problemContainer.innerHTML = problems[today];
 solutionContainer.innerHTML = solutions[today];
+
+solutionButton.onclick = function() {
+    if (solutionContainer.style.display == "none") {
+        solutionContainer.style.display = "initial";
+    } else {
+        solutionContainer.style.display = "none";
+    }
+};
